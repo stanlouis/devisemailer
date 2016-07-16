57ef9597da563cfbb3c4da6a0cbf1afea400d08e
Stanley Louis <stanlouis@msn.com>
2016-07-16 06:51:38 -0700

    add sass style gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 end
